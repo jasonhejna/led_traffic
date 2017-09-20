6c08daf273b406260ba11dbeb33cc0c41b2fde6c
Jason Hejna <jason.hejna@gmail.com>
2017-09-19 23:53:20 -0400

    final led update code

--- getRBG.js
+++ getRBG.js
@@ -1,3 +1,4 @@
+// Uses webshot and phantom js to create image from supplied websites
 //https://github.com/brenden/node-webshot
 
 var webshot = require('webshot');
@@ -9,8 +10,6 @@ var options = {
   }
 }
 var Jimp = require("jimp");
-const rgbHex = require('rgb-hex');
-
 
 function createImage() {
   webshot('http://localhost:8080/index.html', 'yomap.png', options, function (err) {
@@ -18,25 +17,9 @@ function createImage() {
       console.log(err);
       return;
     }
-    findrgb();
     // screenshot now saved to yomap.png
+    // now call python script to update the LED strup
+    
   });
 }
-
-function findrgb(){
-  Jimp.read("./yomap.png").then(function (image) {
-    // do stuff with the image
-    var x = 540;
-    var y = 69;
-    var pixelColor = image.getPixelColor(x, y);
-    console.log('pixelColor', pixelColor);
-    var pixelColorRGB = Jimp.intToRGBA(pixelColor);
-    console.log('pixelColorRGB',pixelColorRGB);
-    console.log('r',pixelColorRGB.r);
-    console.log('hex:',rgbHex(pixelColorRGB.r, pixelColorRGB.g, pixelColorRGB.b));
-  }).catch(function (err) {
-    // handle an exception
-    console.log(err);
-  });
-}
-findrgb();
+createImage();
--- led_control/updateled.py
+++ led_control/updateled.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
+# Import image, and sample colors in various locations
+# This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+
 import Image
 from dotstar import Adafruit_DotStar
+from pixel_coord_data import main_strand
 
 numpixels = 180 # Number of LEDs in strip
 
 # Load image in RGB format and get dimensions:
-filename  = "../yomap.png" # Image file to load
+filename  = "yomap.png" # Image file to load
 img       = Image.open(filename).convert("RGB")
 imgpixels    = img.load()
 
@@ -23,13 +27,9 @@ gamma = bytearray(256)
 for i in range(256):
 	gamma[i] = int(pow(float(i) / 255.0, 2.7) * 255.0 + 0.5)
 
-# This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+pixels = main_strand()
 
-#color = 0xFF0000        # 'On' color (starts red)
-
-pixels = [[1323,346],[1323,342],[1323,338],[1324,331],[1325,326],[1326,321],[1326,316],[1326,307],[1324,299],[1323,295],[1322,290],[1321,284],[1320,277],[1319,270],[1318,263],[1317,257],[1317,252],[1317,248],[1317,245],[1316,224],[1316,222],[1315,217],[1314,212],[1314,209],[1311,204],[1309,201],[1306,198],[1302,194],[1298,190],[1294,185],[1289,182],[1284,178],[1280,174],[1275,170],[1271,165],[1267,161],[1263,157],[1259,153],[1255,149],[1250,147],[1246,143],[1241,139],[1236,136],[1235,134],[1231,132],[1228,129],[1226,125],[1209,106],[1206,102],[1203,99],[1199,95],[1196,92],[1192,88],[1185,85],[1180,84],[1175,83],[1170,82],[1164,82],[1158,82],[1153,82],[1144,83],[1137,82],[1128,82],[1121,82],[1108,82],[1099,82],[1091,82],[1084,83],[1076,85]]
-
-# For each pixel
+# For each LED pixel
 for i in range(numpixels):
     if i < 66:
         value = imgpixels[pixels[i][0], pixels[i][1]]   # Read pixel in image
